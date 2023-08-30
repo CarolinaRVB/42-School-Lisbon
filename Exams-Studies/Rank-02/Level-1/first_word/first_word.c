5022997b1cc4b845aece3a8dff954acd6be7e50b
CarolinaRVB <carolinavbastos@hotmail.com>
2023-08-30 09:47:28 +0100

    exercise correction

--- Exams-Studies/Rank-02/Level-1/first_word/first_word.c
+++ Exams-Studies/Rank-02/Level-1/first_word/first_word.c
@@ -6,7 +6,7 @@
 /*   By: crebelo- <crebelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 14:13:33 by crebelo-          #+#    #+#             */
-/*   Updated: 2023/08/30 09:02:01 by crebelo-         ###   ########.fr       */
+/*   Updated: 2023/08/30 09:28:17 by crebelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,11 @@ int	main(int argc, char *argv[])
 
 	int	i = 0;
 
-	while (argv[1][i] == ' ')
+	while (argv[1][i] < 33 && argv[1][i] > 126)
 		i++;
-	while (argv[1][i] != '\0' && argv[1][i] != ' ')
+	while (argv[1][i] != '\0' && argv[1][i] >= 33 && argv[1][i] <= 126)
 		write(1, &argv[1][i++], 1);
 	write(1, "\n", 1);
+
 	return (0);
 }
