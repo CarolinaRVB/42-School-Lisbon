1c04a2ebb0d5ef31806beb4dc2017c73597220ef
CarolinaRVB <carolinavbastos@hotmail.com>
2023-08-31 13:58:00 +0100

    corrected rot 13 exercise

--- Exams-Studies/Rank-02/Level-1/rot_13/rot_13.c
+++ Exams-Studies/Rank-02/Level-1/rot_13/rot_13.c
@@ -24,9 +24,25 @@ int	main(int argc, char *argv[])
 	int		i = 0;
 
 	while (argv[1][i] != '\0')
-	{
-		c = argv[1][i] + 13;
-		write(1, &c, 1);
+	{	
+		if (argv[1][i]>= 'a' && argv[1][i] <= 'z')
+		{
+			if (argv[1][i] > 'm')
+				c = (97 + ((argv[1][i] + 13) - 123));
+			else
+				c = argv[1][i] + 13;
+			write(1, &c, 1);
+		}
+		else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+		{
+			if (argv[1][i] > 'M')
+				c = (65 + ((argv[1][i] + 13) - 91));
+			else
+				c = argv[1][i] + 13;
+			write(1, &c, 1);
+		}
+		else
+			write(1, &argv[1][i], 1);
 		i++;
 	}
 	write(1, "\n", 1);
