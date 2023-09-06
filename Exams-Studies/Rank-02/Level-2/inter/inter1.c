7017c1c2c35f2e8e0f3ddb22929200a524ac94f0
CarolinaRVB <carolinavbastos@hotmail.com>
2023-09-06 12:56:15 +0100

    new exercises

--- Exams-Studies/Rank-02/Level-2/inter/inter1.c
+++ Exams-Studies/Rank-02/Level-2/inter/inter1.c
@@ -5,53 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: crebelo- <crebelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/05 14:41:26 by crebelo-          #+#    #+#             */
-/*   Updated: 2023/09/05 14:41:26 by crebelo-         ###   ########.fr       */
+/*   Created: 2023/08/08 11:35:20 by crebelo-          #+#    #+#             */
+/*   Updated: 2023/08/08 11:35:20 by crebelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
-
-int	check_doubles(char	*str, char c, int n)
-{
-	int	i = 0;
-	while (str[i] != '\0' && i < n)
-	{
-		if (str[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
+#include<string.h>
+#include <stdio.h>
 
 int	main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		write(1, "\n", 1);
+		write (1, "\n", 1);
 		return (0);
 	}
+
 	int	i = 0;
 	int	j;
+
 	while (argv[1][i] != '\0')
 	{
 		j = 0;
-		if (check_doubles(argv[1], argv[1][i], i) == 0)
+		while (argv[2][j] != '\0')
 		{
-			if(argv[1][i] == '\0')
-				break ;
-			while (argv[2][j] != '\0')
+			if (argv[1][i] == argv[2][j])
 			{
-				if (argv[1][i] == argv[2][j])
-				{
-					write(1, &argv[1][i], 1);
-					break ;
-				}	
-				j++;
+				write(1, &argv[1][i], 1);
+				break; 
 			}
+			j++;
 		}
 		i++;
+		j = 0;
+		while (argv[1][j] != '\0' && argv[1][i] != '\0' && j < i)
+		{
+			if (argv[1][j] == argv[1][i])
+			{
+				i++;
+				break;
+			}
+			j++;
+		}
+
 	}
-	write(1, "\n", 1);
+	write (1, "\n", 1);
 	return (0);
 }
