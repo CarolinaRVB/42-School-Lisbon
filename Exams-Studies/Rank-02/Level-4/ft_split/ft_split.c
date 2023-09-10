905050b2fe13f268106611f4d20d1272a2d9ea58
CarolinaRVB <carolinavbastos@hotmail.com>
2023-09-10 21:16:21 +0100

    add ft split corrected

--- Exams-Studies/Rank-02/Level-4/ft_split/ft_split.c
+++ Exams-Studies/Rank-02/Level-4/ft_split/ft_split.c
@@ -5,84 +5,85 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: crebelo- <crebelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/05 16:26:21 by crebelo-          #+#    #+#             */
-/*   Updated: 2023/09/05 16:26:21 by crebelo-         ###   ########.fr       */
+/*   Created: 2023/09/10 20:26:04 by crebelo-          #+#    #+#             */
+/*   Updated: 2023/09/10 20:26:04 by crebelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
 #include<stdlib.h>
-
+#include<unistd.h>
+#include<stdio.h>
 
 int	count_words(char *str)
 {
 	int	i = 0;
-	int	count = 0;
-	while(str[i] != '\0')
+	int	word = 0;
+	while (str[i] != '\0')
 	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+		while (str[i] != '\0' && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'))
 			i++;
-		if (str[i])
-			count++;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+		if (str[i] != '\0' && str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
+			word++;
+		while (str[i] != '\0' && str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
 			i++;
 	}
-	return (count);
-}
-
-char	*add_word(char *str, int start, int end)
-{
-	int	i = 0;
-	
-	char	*word = (char *)malloc(sizeof(char *) * (end - start + 1));
-	
-	while (str[start] != '\0' && start < end)
-	{
-		word[i] = str[start];
-		i++;
-		start++;
-	}
-	word[i] = '\0';
-
 	return (word);
 }
 
-char    **ft_split(char *str)
+char	*add_word(char *str, int start, int i)
 {
-	int	nwords = count_words(str);
-	int	i = 0;
-	int	j = 0;
-	char	**split = (char **)malloc(sizeof(char *) * (nwords + 1));
-	if (!split)
-		return (NULL);
-
-	while (str[i] != '\0')
+	int 	j = i - start;
+	char	*word = (char *)malloc(sizeof(char *) * j + 1);
+	int	n = 0;
+	
+	while (str[start] != '\0' && start <= i)
 	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-			i++;
-		int start = i;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
-			i++;
-		split[j] = add_word(str, start, i);
-		j++;
-		i++;
+		word[n] = str[start];
+		n++;
+		start++;
 	}
-	split[j] = NULL;
-	return (split);
+	word[n] = '\0';
+	return (word);
 }
 
-// int main() {
-// 	char *input = "This is a sample string for splitting"; // Replace with your own input string
+char	**ft_split(char *str)
+{
+	int nwords = count_words(str);
+	int	i = 0;
+	int	j = 0;
+	int start = 0;
+	char **ar;
+
+	ar = malloc(sizeof(char *) * nwords + 1);
+	if (!ar)
+		return (NULL);
 	
-// 	char **result = ft_split(input);
+	while (str[i] != '\0' && j < nwords)
+	{
+		while (str[i] != '\0' && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'))
+			i++;
+		start = i;
+		while (str[i] != '\0' && str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
+			i++;
+		i--;
+		ar[j] = add_word(str, start, i);
+		j++;
+		i++;		
+	}
+	ar[j] = NULL;
+	return (ar);
+}
 
-// 	// Print the split strings
-// 	for (int i = 0; result[i] != NULL; i++) {
-// 		printf("%s\n", result[i]);
-// 		free(result[i]);  // Free memory allocated for each split string
+// int	main()
+// {
+// 	char	*str = "  starting and ending   " ;
+// 	char	**split = ft_split(str);
+// 	int	i = 0;
+
+// 	while (split[i] != NULL)
+// 	{
+// 		printf("%s\n", split[i]);
+// 		i++;
 // 	}
-
-// 	free(result);  // Free memory allocated for the array of split strings
-
-// 	return 0;
+	
 // }
