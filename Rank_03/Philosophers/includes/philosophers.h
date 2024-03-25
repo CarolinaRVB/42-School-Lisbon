c460dc5a77f7933c14ff5f0086d0f7de2a673fa7
CarolinaRVB <carolinavbastos@hotmail.com>
2024-03-25 09:05:53 +0000

    philosophers

---
+++ Rank_03/Philosophers/includes/philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crebelo- <crebelo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 22:21:35 by crebelo-          #+#    #+#             */
+/*   Updated: 2024/03/25 09:02:45 by crebelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+#include <stdio.h>
+
+#include <pthread.h>
+#include <mutex.h>
+
+#endif
